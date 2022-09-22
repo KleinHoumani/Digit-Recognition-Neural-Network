71f08478286fc9f3d3c917faeecdff08169c3c5d
Klein Houmani <kleinhoumani@gmail.com>
2022-09-21 23:35:11 -0500

    added grid

--- draw_number.py
+++ draw_number.py
@@ -1,31 +1,86 @@
 import pygame
 
-black = (0, 0, 0)
-grey = (200, 200, 215)
-# class pixel(object):
-#     def __init__(self, x, y, width, height):
-#         self.x = x
-#         self.y = y
-#         self.width = width
-#         self.height = height
-#         self.color = (255, 255, 255)
-#         self.neighbors = []
-#
-#     def draw(self, surface):
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-width, height = 900, 500
-size = [width, height]
+FPS = 240
 
-WIN = pygame.display.set_mode((width, height))
+WIDTH = 800
+HEIGHT = 900
+TOOLBAR_HEIGHT = HEIGHT - WIDTH
+
+ROWS = COLS = 28
+
+PIXEL_SIZE = WIDTH // ROWS
+
+BACKGROUND_COLOR = WHITE
+DRAW_COLOR = BLACK
+
+DRAW_GRID_LINES = False
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Number Guesser")
 
+
+def get_font(size):
+    return pygame.font.SysFont("comicsans", size)
+
+
+def draw(win):
+    win.fill(BACKGROUND_COLOR)
+    pygame.display.update()
+
+
+def create_grid(rows, cols, color):
+    grid = []
+
+    for i in range(rows):
+        grid.append([])
+        for j in range(cols):
+            grid[i].append(color)
+
+
+def draw_grid(win):
+    for i in range(ROWS + 1):
+        pygame.draw.line(win, BLACK, (0, i * PIXEL_SIZE), (WIDTH, i * PIXEL_SIZE))
+    for i in range(COLS + 1):
+        pygame.draw.line(win, BLACK, (i * PIXEL_SIZE, 0), (i * PIXEL_SIZE, HEIGHT - TOOLBAR_HEIGHT))
+
+
+
 def main():
+    clock = pygame.time.Clock()
     run = True
+    mouse_down = False
+
+    draw(WIN)
+    grid = create_grid(ROWS, COLS, WHITE)
+    draw_grid(WIN)
+    pygame.display.update()
+
     while run:
+        clock.tick(FPS)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_down = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mouse_down = False
+
+            if mouse_down:
+                x, y = pygame.mouse.get_pos()
+                row = x // PIXEL_SIZE
+                col = y // PIXEL_SIZE
+
+                if row < ROWS:
+                    grid[row][col] = DRAW_COLOR
+
+                pygame.draw.circle(WIN, BLACK, (x, y), 5)
+                pygame.display.update()
+
     pygame.quit()
 
 
--- train_model.py
+++ train_model.py
@@ -2,13 +2,20 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import time
 
 
 mnist = tf.keras.datasets.mnist
 (train, labels_train), (test, labels_test) = mnist.load_data()
 
-letters_train = tf.keras.utils.normalize(train, axis=1)
-letters_test = tf.keras.utils.normalize(test, axis=1)
+
+train = tf.keras.utils.normalize(train, axis=1)
+test = tf.keras.utils.normalize(test, axis=1)
+
+print(train[0])
+plt.imshow(train[0])
+plt.show()
+time.sleep(100)
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten())
@@ -20,7 +27,6 @@ model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=
 model.fit(train, labels_train, epochs=5)
 model.save('num_reader')
 
-val_loss, val_acc = model.evaluate(letters_test, labels_test)
+val_loss, val_acc = model.evaluate(test, labels_test)
 print('loss', val_loss, 'accuracy', val_acc)
 print('Finished training')
-
